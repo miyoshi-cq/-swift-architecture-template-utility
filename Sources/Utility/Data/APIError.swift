7eb48ab59af1d19588ea45d09b96788a4495ff5f
miyoshi-cq <t.miyoshi@caraquri.com>
2022-09-26 19:45:14 +0900

    remove LocalizedError

--- Sources/Utility/Data/APIError.swift
+++ Sources/Utility/Data/APIError.swift
@@ -1,27 +1,10 @@
 import Foundation
 
-public enum APIError: LocalizedError, Equatable {
+public enum APIError: Equatable, Error {
     case unknown
     case missingTestJsonDataPath
     case invalidRequest
     case offline
     case decodeError(String)
     case responseError(statusCode: Int)
-
-    public var errorDescription: String? {
-        switch self {
-        case .unknown:
-            return "unknown error occured"
-        case .missingTestJsonDataPath:
-            return "missing test json data path"
-        case .invalidRequest:
-            return "invalid request"
-        case .offline:
-            return "offline error occured"
-        case let .decodeError(error):
-            return "decode error occured, \(error)"
-        case .responseError:
-            return "response error occured"
-        }
-    }
 }
