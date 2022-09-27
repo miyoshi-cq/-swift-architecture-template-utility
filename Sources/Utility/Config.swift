43e50455741c1c24a209f26ede4915808b69f762
miyoshi-cq <t.miyoshi@caraquri.com>
2022-09-27 12:17:08 +0900

    create MockURLProtocol

--- Sources/Utility/Config.swift
+++ Sources/Utility/Config.swift
@@ -1,7 +1,11 @@
 import Foundation
 
 public enum UtilityConfig {
-    public static func setup(analytics: AnalyticsProvider) {
+    public static func setup(
+        analytics: AnalyticsProvider,
+        fakeAPIErrorStatusCode: Int?
+    ) {
         Analytics.shared.provider = analytics
+        APIClient.fakeAPIErrorStatusCode = fakeAPIErrorStatusCode
     }
 }
