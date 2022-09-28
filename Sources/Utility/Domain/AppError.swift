2dbc77ce6845e4b27bd0bfb91255099309103e44
miyoshi-cq <t.miyoshi@caraquri.com>
2022-09-28 17:10:00 +0900

    rename

--- Sources/Utility/Domain/AppError.swift
+++ Sources/Utility/Domain/AppError.swift
@@ -3,7 +3,7 @@ import Foundation
 public enum AppError: Error, Equatable, LocalizedError {
     case normal(title: String, message: String),
          auth(title: String, message: String),
-         validation(title: String, message: String),
+         notice(title: String, message: String),
          none
 
     public var errorDescription: String? {
@@ -12,7 +12,7 @@ public enum AppError: Error, Equatable, LocalizedError {
             return message
         case let .auth(_, message):
             return message
-        case let .validation(_, message):
+        case let .notice(_, message):
             return message
         case .none:
             return nil
