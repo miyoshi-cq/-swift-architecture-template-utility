6a9fb30fd8efc2b251b0fa34eefeec5a27e872a3
miyoshi-cq <t.miyoshi@caraquri.com>
2022-12-08 10:23:25 +0900

    modify log

--- Sources/Utility/Logger.swift
+++ Sources/Utility/Logger.swift
@@ -8,7 +8,58 @@ public class LogService {
         function: String = #function
     ) {
         #if DEBUG
-        self.logger.log(level: .default, "❗️[Default] \(function, privacy: .public) \(message, privacy: .public)")
+        self.logger.log(
+            level: .default,
+            "❗️[Default] \(function, privacy: .public) \(message, privacy: .public)"
+        )
+        #endif
+    }
+
+    public static func info(
+        _ message: String,
+        function: String = #function
+    ) {
+        #if DEBUG
+        self.logger.log(
+            level: .info,
+            "❗️[Info] \(function, privacy: .public) \(message, privacy: .public)"
+        )
+        #endif
+    }
+
+    public static func debug(
+        _ message: String,
+        function: String = #function
+    ) {
+        #if DEBUG
+        self.logger.log(
+            level: .debug,
+            "❗️[Debug] \(function, privacy: .public) \(message, privacy: .public)"
+        )
+        #endif
+    }
+
+    public static func error(
+        _ message: String,
+        function: String = #function
+    ) {
+        #if DEBUG
+        self.logger.log(
+            level: .error,
+            "❗️[Error] \(function, privacy: .public) \(message, privacy: .public)"
+        )
+        #endif
+    }
+
+    public static func fault(
+        _ message: String,
+        function: String = #function
+    ) {
+        #if DEBUG
+        self.logger.log(
+            level: .fault,
+            "❗️[Fault] \(function, privacy: .public) \(message, privacy: .public)"
+        )
         #endif
     }
 }
