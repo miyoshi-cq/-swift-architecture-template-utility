1be1074048a35a7ddabe41d4c23812c903d53662
miyoshi-cq <t.miyoshi@caraquri.com>
2022-12-21 00:32:53 +0900

    cleanup

--- Sources/Utility/Date.swift
+++ Sources/Utility/Date.swift
@@ -1,71 +1,91 @@
 import Foundation
 
-public extension DateFormatter {
-    static func create(_ format: String) -> DateFormatter {
+extension TimeZone {
+    static let japan = TimeZone(identifier: "Asia/Tokyo")!
+}
+
+extension Locale {
+    static let japan = Locale(identifier: "ja_jp")
+}
+
+public enum DateFormat: String {
+    case dashDate = "yyyy-MM-dd"
+    case dashDateTime = "yyyy-MM-dd HH:mm:ss"
+    case slashDate = "yyyy/MM/dd"
+    case slashDateTime = "yyyy/MM/dd HH:mm:ss"
+    case dotDate = "yyyy.MM.dd"
+    case yyyyMdDate = "yyyy.M.d"
+    case yyyyMdHmDate = "yyyy.MM.dd HH:mm"
+    case dotDateTime = "yyyy.MM.dd HH:mm:ss"
+    case ISODateTime = "yyyy-MM-dd'T'HH:mm:ssZ"
+    case ISODateTimeMilliSecond = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+    case japanese = "yyyy年MM月dd日"
+
+    var formatter: DateFormatter {
         let formatter = DateFormatter()
-        formatter.locale = Locale(identifier: "ja_JP")
-        formatter.dateFormat = format
+        formatter.timeZone = .japan
+        formatter.locale = .japan
+        formatter.dateFormat = self.rawValue
 
         var calender = Calendar(identifier: .gregorian)
         calender.locale = .current
         formatter.calendar = calender
         return formatter
     }
+}
 
-    static var iso8601: ISO8601DateFormatter {
-        ISO8601DateFormatter()
-    }
+public enum ISO8601DateFormat {
+    case iso8601
+    case iso8601WithSeconds
+    case iso8601withFractionalSeconds
 
-    static var iso8601WithSeconds: ISO8601DateFormatter {
-        let f = ISO8601DateFormatter()
-        f.formatOptions = [
-            .withFullDate,
-            .withTime,
-            .withTimeZone,
-            .withDashSeparatorInDate,
-            .withColonSeparatorInTime,
-        ]
-        return f
-    }
+    var formatter: ISO8601DateFormatter {
+        switch self {
+        case .iso8601:
+            return ISO8601DateFormatter()
 
-    static var iso8601withFractionalSeconds: ISO8601DateFormatter {
-        let f = ISO8601DateFormatter()
-        f.formatOptions = [
-            .withFullDate,
-            .withTime,
-            .withTimeZone,
-            .withDashSeparatorInDate,
-            .withColonSeparatorInTime,
-            .withFractionalSeconds,
-        ]
-        return f
+        case .iso8601WithSeconds:
+            let f = ISO8601DateFormatter()
+            f.formatOptions = [
+                .withFullDate,
+                .withTime,
+                .withTimeZone,
+                .withDashSeparatorInDate,
+                .withColonSeparatorInTime,
+            ]
+            return f
+
+        case .iso8601withFractionalSeconds:
+            let f = ISO8601DateFormatter()
+            f.formatOptions = [
+                .withFullDate,
+                .withTime,
+                .withTimeZone,
+                .withDashSeparatorInDate,
+                .withColonSeparatorInTime,
+                .withFractionalSeconds,
+            ]
+            return f
+        }
     }
 }
 
 public extension String {
-    var iso8601: Date? {
-        DateFormatter.iso8601.date(from: self)
+    func date(from format: DateFormat) -> Date? {
+        format.formatter.date(from: self)
     }
 
-    var iso8601WithSeconds: Date? {
-        DateFormatter.iso8601WithSeconds.date(from: self)
-    }
-
-    var iso8601withFractionalSeconds: Date? {
-        DateFormatter.iso8601withFractionalSeconds.date(from: self)
+    func date(from format: ISO8601DateFormat) -> Date? {
+        format.formatter.date(from: self)
     }
 }
 
 public extension Date {
-    var iso8601: String {
-        DateFormatter.iso8601.string(from: self)
+    func string(to format: DateFormat) -> String {
+        format.formatter.string(from: self)
     }
 
-    var iso8601WithSeconds: String {
-        DateFormatter.iso8601WithSeconds.string(from: self)
-    }
-
-    var iso8601withFractionalSeconds: String {
-        DateFormatter.iso8601withFractionalSeconds.string(from: self)
+    func string(to format: ISO8601DateFormat) -> String {
+        format.formatter.string(from: self)
     }
 }
