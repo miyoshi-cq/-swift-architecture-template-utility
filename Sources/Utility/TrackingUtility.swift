9726980dddcdcfa8fbab44ce976fa2a754de97b8
miyoshi-cq <t.miyoshi@caraquri.com>
2023-06-06 13:05:45 +0900

    add TrackingUtility

---
+++ Sources/Utility/TrackingUtility.swift
@@ -0,0 +1,24 @@
+import AppTrackingTransparency
+
+public final class TrackingUtility {
+    public static func requestPermission() {
+        if #available(iOS 14, *) {
+            switch ATTrackingManager.trackingAuthorizationStatus {
+            case .notDetermined:
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                    ATTrackingManager.requestTrackingAuthorization { _ in }
+                }
+            case .restricted:
+                break
+            case .denied:
+                break
+            case .authorized:
+                break
+            @unknown default:
+                break
+            }
+        } else {
+            // Fallback on earlier versions
+        }
+    }
+}
